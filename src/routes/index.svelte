85587a7c01ca1a7fdbd4836f75721a5d504cc098
cabralpinto <jmcabralpinto@gmail.com>
2022-07-22 18:50:23 +0100

    added text line with offset variable

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,1 +1,16 @@
-<h1 class="text-3xl font-bold underline">Hello world!</h1>
+<script>
+	let offset = 10;
+</script>
+
+<main class="flex h-screen items-center overflow-hidden">
+	<p
+		class="whitespace-nowrap font-serif text-7xl selection:bg-purple-50 relative"
+		style="right: {offset}cm"
+	>
+		Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+		labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+		laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+		voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+		non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+	</p>
+</main>
